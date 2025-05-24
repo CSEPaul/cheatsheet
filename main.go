f114e2ffa63bc7551dccc5cf25a1e1b5fa76073d
paul <paul@pt515>
2025-05-24 12:29:28 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
-func main() {
+func Ps() {
+	fmt.Println(" ")
+}
 
+func Pv(value string) {
+	fmt.Println("----", value, "-----")
+}
+
+func main() {
+	Pv("strings and numbers")
 	fmt.Println("test print strings")
 	fmt.Println("Numbers", 123)
 
+	Ps()
+	Pv("Arrays")
 	// Array of length 10
 	fmt.Println("Array of length 10")
 	var a [10]int
@@ -37,5 +48,40 @@ func main() {
 	fmt.Printf("a\t%v\n", c)
 	fmt.Println("length of map", len(c))
 	fmt.Printf("b\t%v\n", d)
+
+	// structs
+	type people struct {
+		age  string
+		name string
+	}
+
+	var p people
+	p.age = "34"
+	p.name = "meme"
+	fmt.Println(p)
+
+	// Marshal the data
+	peopledata, err := json.Marshal(p)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Marshalled data")
+	fmt.Println(peopledata)
+
+	fmt.Println(" ")
+	fmt.Println(" ")
+	// Marshal example 2
+
+	type User struct {
+		Name        string
+		Age         int
+		Active      bool
+		lastLoginAt string
+	}
+	u, err := json.Marshal(User{Name: "Bob", Age: 10, Active: true, lastLoginAt: "today"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(u)) // {"Name":"Bob","Age":10,"Active":true}
 
 }
