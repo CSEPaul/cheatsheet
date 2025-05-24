81bb8d11e2bfbf8251991e8fa494b5c68095795b
paul <paul@pt515>
2025-05-24 12:13:28 +0100

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,20 @@ func main() {
 	// Array of length 10 and with values
 	var b = [10]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 	fmt.Println(b)
-	fmt.Println(b[1])
-	fmt.Println(b[-1])
+	fmt.Println("first element", b[1])
+	fmt.Println("length of array", len(b))
+	fmt.Println("-----")
+	fmt.Println("Type of: ")
+	fmt.Printf("%T\n", b)
+	fmt.Println("-----")
+	fmt.Println("last element of array", b[len(b)-1])
+
+	// Maps
+	var c = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+	d := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+
+	fmt.Printf("a\t%v\n", c)
+	fmt.Println("length of map", len(c))
+	fmt.Printf("b\t%v\n", d)
 
 }
