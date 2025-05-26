eeab8b2f70dd3158c2a48a244c89ddf660fd3f3c
paul <paul@pt515>
2025-05-26 03:48:38 +0100

    Update json.go

--- cmd/json.go
+++ cmd/json.go
@@ -148,4 +148,55 @@ func JsonRunner() {
 	}
 	fmt.Print(string(b))
 
+	Pv("REFLECT test ")
+
+	//var products = fake2.Products
+	/*for _, value := range products {
+	var reviews = (value.Reviews)
+	for _, rat := range reviews {
+		var ratings = rat.Rating
+		if ratings == 4 {
+
+		}
+	}
+	}
+	*/
+
+	t := reflect.TypeOf(fake2)
+	fmt.Println(t)
+	v := reflect.ValueOf(fake2)
+
+	if t.Kind() != reflect.Struct {
+		fmt.Println("Not a struct, check your input")
+		return
+	}
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		value := v.Field(i)
+
+		fmt.Printf("Field Name: %s, Field Type: %s, Field Value: %v\n",
+			field.Name, field.Type, value)
+
+	}
+
+	Pv("Alternative method")
+	// Get the reflection value of an object.
+	rValue := reflect.ValueOf(fake2)
+
+	// Get the reflection kind of an object.
+	rKind := rValue.Kind()
+	fmt.Println(rKind)
+
+	// Get the reflection type of an object (two options).
+	rType := reflect.TypeOf(fake2)
+	//rType := rValue.Type()
+
+	//Alertnative method
+	if rType.Kind() == reflect.Struct {
+		for i := 0; i < rType.NumField(); i++ {
+			fieldValue := rValue.Field(i)
+			fmt.Println(fieldValue)
+		}
+
+	}
 }
